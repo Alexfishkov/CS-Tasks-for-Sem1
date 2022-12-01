d90c291e77a20f8ab91315cbca38d3a71af9d149
Alexfishkov <afscorp@mail.ru>
2022-12-01 12:10:45 +0300

    Last figure in your number

---
+++ Task7/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Указываем последнюю цифру введённого Вами числа");
+Console.Write("Введите ваше число>");
+int number = Int32.Parse(Console.ReadLine());
+int lastfigure = (number%10);
+Console.WriteLine("Последней цифрой Вашего числа является: " + lastfigure);
+
