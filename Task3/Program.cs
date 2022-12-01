d755f7540150e7088e7d7c7f2ad121df245c249d
Alexfishkov <afscorp@mail.ru>
2022-12-01 11:19:10 +0300

    Name of week day by the number

---
+++ Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Определяем по введённому номеру дня недели его название");
+string[] dweeks = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+Console.Write("Введите номер дня недели (от 1 до 7)=>");
+int yournumber = Int32.Parse(Console.ReadLine());
+int daynumber = yournumber-1;
+Console.WriteLine("Это " + dweeks[daynumber]);
