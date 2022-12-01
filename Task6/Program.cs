c12ddb20bb227cd33318be936901be876baeefa0
Alexfishkov <afscorp@mail.ru>
2022-12-01 12:01:00 +0300

    Even numbers

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Определяем чётность введённого Вами числа");
+Console.Write("Введите ваше число>");
+int number = Int32.Parse(Console.ReadLine());
+if ((number % 2) != 0)
+{
+    Console.WriteLine("Введённое число является нечётным");
+}
+else
+{
+    Console.WriteLine("Введённое число является чётным");
+}
