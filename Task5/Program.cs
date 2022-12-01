28f87ee81471a136ef84cc79f055fc9ea10313d3
Alexfishkov <afscorp@mail.ru>
2022-12-01 11:49:31 +0300

    Print numbers from -N to N

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Распечатываем все значения целых чисел от -N до N");
+Console.Write("Введите N=>");
+int n = Int32.Parse(Console.ReadLine());
+int i = -n;
+while (i <= n)
+{
+    Console.Write(i + ", ");
+    i++;
+}
+Console.WriteLine();
+
