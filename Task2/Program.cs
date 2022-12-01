2a14b878e95c4d4146fac4f32b9973ad16cce270
Alexfishkov <afscorp@mail.ru>
2022-12-01 10:55:27 +0300

    Comparing of two numbers

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Определяем, какое из двух введённых числе Max, а какое Min");
+Console.Write("Введите первое число=>");
+int firstnumber = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число=>");
+int secondnumber = Int32.Parse(Console.ReadLine());
+if (secondnumber == firstnumber)
+{
+    Console.WriteLine("Числа равны, Min=Max");
+}
+else if (secondnumber > firstnumber)
+{
+    Console.WriteLine("Max= " + secondnumber + " Min= " + firstnumber);
+}
+else
+{
+    Console.WriteLine("Max= " + firstnumber + " Min= " + secondnumber);
+}
