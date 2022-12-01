bce00d527cc64d272011a82b33a1d6685ebb3f86
Alexfishkov <afscorp@mail.ru>
2022-12-01 12:24:44 +0300

    All even numbers up to N

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Выводим все чётные числа в промежутке от 1 до введённого Вами положительного числа");
+Console.Write("Введите Ваше число=>");
+int yournumber = Int32.Parse(Console.ReadLine());
+int i = 1;
+while (i <= yournumber)
+{
+    if ((i % 2) == 0)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+}
+Console.WriteLine(" ");
