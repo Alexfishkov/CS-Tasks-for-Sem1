1e8e477b90e0f9ed351fcfd513552e56123628ea
Alexfishkov <afscorp@mail.ru>
2022-12-01 10:33:53 +0300

    square test

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Определяем, является ли первое число квадратом второго");
+Console.Write("Введите первое число=>");
+int firstnumber = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число=>");
+int secondnumber = Int32.Parse(Console.ReadLine());
+if ((secondnumber * secondnumber) == firstnumber)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
